20cd595f98206106b5642a395ba4ea604baa092c
AlexeiRGut <166068219+AlexeiRGut@users.noreply.github.com.>
2024-06-30 21:53:34 +0200

    porfolio update 30/06/24
    porfolio update 30/06/24

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -9,16 +9,21 @@ window.addEventListener('scroll', function() {
     }
 });
 
+
+
+
+
+
 const menu = document.getElementById('menu')
 const headerUl = document.getElementById('headerUl')
 
 function showHideUl() {
 
-    if(menu.classList.contains('menuClicked')) {
-        menu.classList.remove('menuClicked')
+    if(menu.classList.contains('clickedMenu')) {
+        menu.classList.remove('clickedMenu')
     }
     else {
-        menu.classList.add('menuClicked')
+        menu.classList.add('clickedMenu')
     }
 
     if (headerUl.classList.contains('hiddenUl')) {
@@ -32,25 +37,33 @@ function showHideUl() {
 }
 
 menu.addEventListener('click', showHideUl)
-// menu.addEventListener('focusout', showHideUl)
-
-const navInicio = document.getElementById('navInicio')
-const navProyectos = document.getElementById('navProyectos')
-const navSobreMi = document.getElementById('navSobreMi')
-const navContacto = document.getElementById('navContacto')
 
 
 
 
 
-navInicio.addEventListener('click', function(){
 
+const logo = document.getElementById('logo')
+const navHome = document.getElementById('navHome')
+const navProjects = document.getElementById('navProjects')
+const navAbout = document.getElementById('navAbout')
+const navContact = document.getElementById('navContact')
+
+function movoToStart() {
     window.scroll({ top: 0, behavior: "smooth" })
-})
+}
 
-navSobreMi.addEventListener('click', function() {
-    const sobreMiSection = document.getElementById('sobreMiSection');
-    const yOffset = sobreMiSection.getBoundingClientRect().top + window.scrollY;
+logo.addEventListener('click', movoToStart)
+navHome.addEventListener('click', movoToStart)
+
+
+
+
+
+
+navAbout.addEventListener('click', function() {
+    const aboutMe = document.getElementById('aboutMe');
+    const yOffset = aboutMe.getBoundingClientRect().top + window.scrollY;
     window.scrollTo({ top: yOffset, behavior: 'smooth' });
 });
 
@@ -61,7 +74,7 @@ navSobreMi.addEventListener('click', function() {
 //     window.scrollTo({ top: yOffset, behavior: 'smooth' });
 // })
 
-// navContacto.addEventListener('click', function(){
+// navContact.addEventListener('click', function(){
 //     const contactoSection = document.getElementById('contactoSection');
 //     const yOffset = contactoSection.getBoundingClientRect().top + window.scrollY;
 //     window.scrollTo({ top: yOffset, behavior: 'smooth' });
